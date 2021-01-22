1da48e9c08495234d25f1654302fc6383016b5f0
miadehaan <64979457+miadehaan@users.noreply.github.com>
2021-01-22 12:34:30 -0800

    added HashRouter to allow page refresh

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>
+    <HashRouter>
+        <Router>
+            <App />
+        </Router>
+    </HashRouter>
 , document.getElementById('root') );
 
 
