74b9c005c95ece8beef40e9c1b4e18d00482f6c3
miadehaan <64979457+miadehaan@users.noreply.github.com>
2021-01-26 16:27:26 -0800

    portfolio cards are now mobile responsive, added more content to cards with accordian

--- src/components/ProjectBlock/index.js
+++ src/components/ProjectBlock/index.js
@@ -1,51 +1,61 @@
 import React from 'react';
 import "./style.css";
 import projects from "../../utils/projects.js";
-import { CardColumns, Card, Button } from "react-bootstrap";
+import { Card, Row, Col, Container, Accordion, Button } from "react-bootstrap";
 
 function ProjectBlock() {
     // console.log(projects);
 
     return (
-        <div className="cards">
-            {/* <CardColumns> */}
-            <div className="card-columns">
-                {projects.map( (res, index) => {
-                    // console.log(index);
-                    return(
-                        <Card key={res.key} className="card_container" style={{ width: '18rem' }}>
-                            <Card.Img className="appImage" variant="top" src={res.img} alt={res.alt} />
+        <Container>
+        <Row>
+            {projects.map( (res, index) => {
+                // console.log(index);
+                return(
+                    <Col lg={4} md={6} sm={12} style={{ paddingBottom: '30px' }}>
+                    <Card key={res.key} className="card_container" style={{ width: '100%' }}>
+                        <Card.Img className="appImage" variant="top" src={res.img} alt={res.alt} />
 
-                            <Card.Header> {res.title} </Card.Header>
-                            <Card.Body>
+                        <Card.Header> {res.title} </Card.Header>
+                        <Card.Body>
+                        <Accordion defaultActiveKey="0">
+                            <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                                Show Description
+                            </Accordion.Toggle>
+
+                            <Accordion.Collapse eventKey="1">
                                 <Card.Text>
                                     Description: {res.description}
                                 </Card.Text>
-                                <div >
-                                    Technologies Used:
-                                    <ul>
-                                        {res.technology.map((tech, index) => {
-                                            // console.log(tech);
-                                            return (
-                                                <li key={index}> {tech} </li>
-                                            )
-                                        })}
-                                    </ul>
-                                </div>
-                                
-                                <div className="links">
-                                    <a className="portfolio" href={res.deployedLink} target="blank"> Go to App </a>
-                                    <span> | </span>
-                                    <a className="portfolio" href={res.githubLink} target="blank"> Check out the code! </a>
-                                </div>
+                            </Accordion.Collapse>
+                        </Accordion>
 
-                            </Card.Body>
-                        </Card>
-                    )
-                })}
-            {/* </CardColumns> */}
-            </div>
-        </div>
+                            <div className="techList">
+                                Technologies Used:
+                                <ul>
+                                    {res.technology.map((tech, index) => {
+                                        // console.log(tech);
+                                        return (
+                                            <li key={index}> {tech} </li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
+                            
+                            <div className="links">
+                                <a className="portfolio" href={res.deployedLink} target="blank"> Go to App </a>
+                                <span> | </span>
+                                <a className="portfolio" href={res.githubLink} target="blank"> Check out the code! </a>
+                            </div>
+
+                        </Card.Body>
+                    </Card>
+
+                    </Col>
+                )
+            })}            
+        </Row>
+        </Container>
     )
 }
 
--- src/utils/projects.js
+++ src/utils/projects.js
@@ -3,14 +3,19 @@ const projects =[
         title: "Weekend Getaway",
         img: "Assets/weekendgetaway.PNG",
         alt: "Screenshot of Weekend Getaway Application",
-        description: " ",
+        description: "This app allows the user to find campsites and activities nearby based on the city they search and the activities they filter by. The user can create an account, and once logged in, they are able to save the different campgrounds and activities and create an itinerary. The user can view all of their saved itineraries and get directions to the campground from their current location.",
         technology: [
             "HTML",
             "CSS",
             "JavaScript",
             "Bootstrap",
             "React",
-            "Firebase"
+            "IndexedDB",
+            "Firebase",
+            "Node JS",
+            "Express",
+            "MongoDB",
+            "3rd Party APIs"
         ],
         deployedLink: "https://murmuring-citadel-70969.herokuapp.com/",
         githubLink: "https://github.com/miadehaan/WeekendGetAway",
@@ -20,12 +25,14 @@ const projects =[
         title: "AirWalk",
         img: "Assets/airwalk.PNG",
         alt: "Screenshot of AirWalk application home page with a map",
-        description: " ",
+        description: "An app in which the user search for hikes based on city and can filter by city, distance, elevation, difficulty. A map with pins of each hike is diplsayed, as well as a 5-day weather forecast for the searched area.",
         technology: [
             "HTML",
             "CSS",
+            "W3.CSS",
             "JavaScript",
-            "Bootstrap"
+            "jQuery",
+            "3rd Party APIs"
         ],
         deployedLink: "https://mx6.github.io/Bootcamp_Project_1/",
         githubLink: "https://github.com/mx6/Bootcamp_Project_1",
@@ -35,7 +42,27 @@ const projects =[
         title: "Foodies Paradise",
         img: "Assets/foodies.PNG",
         alt: "Screenshot of Foodies Paradise application",
+        description: "Using the user's geolocation, 10 restaurants nearby will populate with the restaurant's information. The user has options to either view dishes that have been reviewed by other people, or they can leave their own dish review. The search functions for this application do not currently work, however, future development for this project will incorporate that.",
+        technology: [
+            "HTML",
+            "CSS",
+            "Bootstrap",
+            "JavaScript",
+            "jQuery",
+            "Handlebars",
+            "Node JS",
+            "Express.js",
+            "MySQL",
+            "Sequelize"
+        ],
         deployedLink: "https://frozen-river-52540.herokuapp.com/",
+        githubLink: "https://github.com/miadehaan/FoodiesParadise.io",
+        key: 3
+    },
+    {
+        title: "Daily Planner",
+        img: "Assets/planner.PNG",
+        alt: "Screenshot of a daily planner application",
         description: " ",
         technology: [
             "HTML",
@@ -43,17 +70,10 @@ const projects =[
             "JavaScript",
             "Bootstrap"
         ],
-        githubLink: "https://github.com/miadehaan/FoodiesParadise.io",
-        key: 3
+        deployedLink: "https://miadehaan.github.io/Daily_Planner/",
+        githubLink: "https://github.com/miadehaan/Daily_Planner",
+        key: 4
     },
-    // {
-    //     title: "Daily Planner",
-    //     img: "Assets/planner.PNG",
-    //     alt: "Screenshot of a daily planner application",
-    //     deployedLink: "https://miadehaan.github.io/Daily_Planner/",
-    //     githubLink: "https://github.com/miadehaan/Daily_Planner",
-    //     key: 4
-    // },
     {
         title: "Note Taker",
         img: "Assets/notetaker.PNG",
